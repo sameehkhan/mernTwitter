0458f7efc6bd3ed83057ee2baa199148af24fd8e
Sameeh <sameehkhan1@gmail.com>
2018-12-27 12:46:25 -0800

    Set up schema for User

--- models/User.js
+++ models/User.js
@@ -1,2 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
+
+
+const UserSchema = new Schema({
+    handle: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
