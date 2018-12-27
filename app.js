7cc703fc5c7e01e520ee02b956c14752aac9b4d1
Sameeh <sameehkhan1@gmail.com>
2018-12-27 11:12:09 -0800

    Add nodemon to watch for changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Sameeh's server"));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
