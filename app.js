661c5adede63ba66367f7c35c3193546002c721b
Sameeh <sameehkhan1@gmail.com>
2018-12-27 12:02:08 -0800

    Import routes into entry

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
 const app = express();
 const db = require('./config/keys').mongoURI;
 
@@ -11,3 +14,4 @@ mongoose
 app.get("/", (req, res) => res.send("Hello All"));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
