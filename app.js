6fce908f997a5986de1eb54a6c3f304baa56c03c
Sameeh <sameehkhan1@gmail.com>
2018-12-27 11:26:40 -0800

    Install nodemon dependency

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Sameeh's server"));
+app.get("/", (req, res) => res.send("Hello All"));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
