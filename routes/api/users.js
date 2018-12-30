b350895ece1903b6bcbd91a87b849a3032bcfae5
Sameeh <sameehkhan1@gmail.com>
2018-12-29 17:54:57 -0800

    Update user register route

--- routes/api/users.js
+++ routes/api/users.js
@@ -56,36 +56,46 @@ router.post('/login', (req, res) => {
 
 
 
-router.post('/register', (req, res) => {
-  // Check to make sure nobody has already registered with a duplicate email
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if (user) {
-        // Throw a 400 error if the email address already exists
-        return res.status(400).json({email: "A user has already registered with this address"})
-      } else {
-        // Otherwise create a new user
-        const newUser = new User({
-          handle: req.body.handle,
-          email: req.body.email,
-          password: req.body.password
-        })
+router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
 
-        // newUser.save().then(user => res.send(user)).catch(err => res.send(err));
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
+  User.findOne({ name: req.body.name }).then(user => {
+    if (user) {
+      errors.name = "User already exists";
+      return res.status(400).json(errors);
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
 
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser.save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err));
-          })
-        })
-      }
-    })
-})
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => {
+              const payload = { id: user.id, name: user.name };
+
+              jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                res.json({
+                  success: true,
+                  token: "Bearer " + token
+                });
+              });
+            })
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 
 module.exports = router;
