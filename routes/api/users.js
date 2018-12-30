e81eac50e6fd417ee2c19b145aa5e23b8107ebfe
Sameeh <sameehkhan1@gmail.com>
2018-12-29 22:02:46 -0800

    Validate input for login

--- routes/api/users.js
+++ routes/api/users.js
@@ -58,13 +58,13 @@ router.post('/register', (req, res) => {
 
 
 router.post('/login', (req, res) => {
-  // const { errors, isValid } = validateLoginInput(req.body);
+  const { errors, isValid } = validateLoginInput(req.body);
 
-  // console.log(errors);
+  console.log(errors);
 
-  // if (!isValid) {
-  //   return res.status(400).json(errors);
-  // }
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
   const email = req.body.email;
   const password = req.body.password;
