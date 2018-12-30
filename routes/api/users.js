5790ea27fceae1e7bad8bf0abf08ca7120f0240b
Sameeh <sameehkhan1@gmail.com>
2018-12-29 17:55:30 -0800

    Update login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -31,26 +31,39 @@ bcrypt.compare(password, user.password)
   })
 
 
-router.post('/login', (req, res) => {
-  const email = req.body.email;
+router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const name = req.body.name;
   const password = req.body.password;
 
-  User.findOne({ email })
-    .then(user => {
-      if (!user) {
-        return res.status(404).json({ email: 'This user does not exist' });
-      }
+  User.findOne({ name }).then(user => {
+    if (!user) {
+      errors.name = "This user does not exist";
+      return res.status(400).json(errors);
+    }
 
-      bcrypt.compare(password, user.password)
-        .then(isMatch => {
-          if (isMatch) {
-            res.json({ msg: 'Success' });
-          } else {
-            return res.status(400).json({ password: 'Incorrect password' });
-          }
-        })
-    })
-})
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        const payload = { id: user.id, name: user.name };
+
+        jwt.sign(payload, keys.secretOrKeys, { expiresIn: 3600 }, (err, token) => {
+          res.json({
+            success: true,
+            token: "Bearer " + token
+          });
+        });
+      } else {
+        errors.password = "Incorrect password";
+        return res.status(400).json(errors);
+      }
+    });
+  });
+});
 
 
 
