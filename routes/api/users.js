96040d61e89788078f709a720aa09c44e40f59d4
Sameeh <sameehkhan1@gmail.com>
2018-12-29 17:53:33 -0800

    Set up payload

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,7 +9,26 @@ const keys = require('../../config/keys');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
+bcrypt.compare(password, user.password)
+  .then(isMatch => {
+    if (isMatch) {
+      const payload = { id: user.id, name: user.name };
 
+      jwt.sign(
+        payload,
+        keys.secretOrKey,
+        // Tell the key to expire in one hour
+        { expiresIn: 3600 },
+        (err, token) => {
+          res.json({
+            success: true,
+            token: 'Bearer ' + token
+          });
+        });
+    } else {
+      return res.status(400).json({ password: 'Incorrect password' });
+    }
+  })
 
 
 router.post('/login', (req, res) => {
