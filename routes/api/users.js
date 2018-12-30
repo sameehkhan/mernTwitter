ffb0c0d50491b870c7f07cfb3fcaa6e8108761b5
Sameeh <sameehkhan1@gmail.com>
2018-12-29 17:31:49 -0800

    Add user to db with salting password

--- routes/api/users.js
+++ routes/api/users.js
@@ -24,15 +24,15 @@ router.post('/register', (req, res) => {
         // newUser.save().then(user => res.send(user)).catch(err => res.send(err));
 
 
-        // bcrypt.genSalt(10, (err, salt) => {
-        //   bcrypt.hash(newUser.password, salt, (err, hash) => {
-        //     if (err) throw err;
-        //     newUser.password = hash;
-        //     newUser.save()
-        //       .then(user => res.json(user))
-        //       .catch(err => console.log(err));
-        //   })
-        // })
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          })
+        })
       }
     })
 })
