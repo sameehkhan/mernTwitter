c3cc5245defad69c2445168d05d5ea58ff319373
Sameeh <sameehkhan1@gmail.com>
2018-12-29 18:08:53 -0800

    Remove extraneous validation statements

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,15 +7,19 @@ const keys = require('../../config/keys');
 const passport = require('passport');
 
 
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json({ msg: 'Success' });
+})
+
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
 router.post('/register', (req, res) => {
-  const { errors, isValid } = validateRegisterInput(req.body);
+  // const { errors, isValid } = validateRegisterInput(req.body);
 
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
+  // if (!isValid) {
+  //   return res.status(400).json(errors);
+  // }
 
   // Check to make sure nobody has already registered with a duplicate email
   User.findOne({ email: req.body.email })
@@ -46,13 +50,13 @@ router.post('/register', (req, res) => {
 
 
 router.post('/login', (req, res) => {
-  const { errors, isValid } = validateLoginInput(req.body);
+  // const { errors, isValid } = validateLoginInput(req.body);
 
-  console.log(errors);
+  // console.log(errors);
 
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
+  // if (!isValid) {
+  //   return res.status(400).json(errors);
+  // }
 
   const email = req.body.email;
   const password = req.body.password;
