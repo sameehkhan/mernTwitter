de3db028981c0f5d704eb646834201b0ef1440d7
Sameeh <sameehkhan1@gmail.com>
2018-12-29 18:06:13 -0800

    Initialize passport

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ mongoose
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(passport.initialize());
+
+// app.get("/", (req, res) => res.send("Hello World"));
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
