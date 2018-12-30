8f4472db62a7291ef3f68603da9667fdcbe5f05d
Sameeh <sameehkhan1@gmail.com>
2018-12-29 17:42:08 -0800

    Add login route for users

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,34 @@ const User = require('../../models/User');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
+
+
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ email: 'This user does not exist' });
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({ msg: 'Success' });
+          } else {
+            return res.status(400).json({ password: 'Incorrect password' });
+          }
+        })
+    })
+})
+
+
+
+
+
 router.post('/register', (req, res) => {
   // Check to make sure nobody has already registered with a duplicate email
   User.findOne({ email: req.body.email })
