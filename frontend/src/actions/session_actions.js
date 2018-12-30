3e779e18e5bca93f882b5af75b65e454d023e64a
Sameeh <sameehkhan1@gmail.com>
2018-12-30 13:41:57 -0800

    Remove comments

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -1,8 +1,6 @@
 import * as APIUtil from '../util/session_api_util';
 import jwt_decode from 'jwt-decode';
 
-// This pattern should be familiar to you from the full stack project
-
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
 
 export const logoutUser = () => ({
@@ -14,6 +12,5 @@ export const logout = () => dispatch => {
     localStorage.removeItem('jwtToken')
     // Remove the token from the common axios header
     APIUtil.setAuthToken(false)
-    // Dispatch a logout action
     dispatch(logoutUser())
 };
