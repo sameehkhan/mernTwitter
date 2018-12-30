61435f5bfcb219987c9a4f8f504aea2894c94493
Sameeh <sameehkhan1@gmail.com>
2018-12-29 17:28:53 -0800

    Test with postman if user can be inserted into DB

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
-const User = require('./models/User')
 
 const app = express();
 const db = require('./config/keys').mongoURI;
