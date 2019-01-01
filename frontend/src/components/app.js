ffbb2805a50537e4d750f67bf844c9afd54a79b5
Sameeh <sameehkhan1@gmail.com>
2019-01-01 14:38:53 -0800

    Add tweet reducer to root

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
-import NavBarContainer from './nav/navbar_container';
+import NavBarContainer from './nav/nav_bar_container';
 
 import TweetsContainer from './tweets/tweets_container';
 import MainPage from './main/main_page';
